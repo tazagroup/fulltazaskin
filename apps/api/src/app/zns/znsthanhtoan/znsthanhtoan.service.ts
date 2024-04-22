fbdcfb8f6c1b4b02951ce642d83418ccbd2a648a
quocbao1983 <quocbao280783@gmail.com>
2024-04-22 23:09:53 +0700

    update

--- apps/api/src/app/zns/znsthanhtoan/znsthanhtoan.service.ts
+++ apps/api/src/app/zns/znsthanhtoan/znsthanhtoan.service.ts
@@ -114,17 +114,17 @@ export class ZnsthanhtoanService {
         else {
           data.Status = 2;
           data.Statuscode = result.error;
-          const resultsms = await this.sendsms({
-            "Brandname": "TAZA",
-            "Message": `${data.CustName} da thanh toan so tien ${data.Paid} co ma hoa don la ${data.Code}. Taza cam on quy khach`,
-            "Phonenumber": data.CustPhone,
-            "user": "ctytaza2",
-            "pass": "$2a$10$QjKAPJ9qq.RuS3jfUID2FeuGdpuSL1Rl9ugQUvy.O5PuKSlp8z95S",
-            "messageId": data.CustPhone + (new Date()).getTime()
-          })
-          console.log(resultsms);
-          data.SMSCode = resultsms.data.status;
-          data.messageId =resultsms.data.messageId;
+          // const resultsms = await this.sendsms({
+          //   "Brandname": "TAZA",
+          //   "Message": `${data.CustName} da thanh toan so tien ${data.Paid} co ma hoa don la ${data.Code}. Taza cam on quy khach`,
+          //   "Phonenumber": data.CustPhone,
+          //   "user": "ctytaza2",
+          //   "pass": "$2a$10$QjKAPJ9qq.RuS3jfUID2FeuGdpuSL1Rl9ugQUvy.O5PuKSlp8z95S",
+          //   "messageId": data.CustPhone + (new Date()).getTime()
+          // })
+          // console.log(resultsms);
+          // data.SMSCode = resultsms.data.status;
+          // data.messageId =resultsms.data.messageId;
           this.update(data.id, data)
         }
 
