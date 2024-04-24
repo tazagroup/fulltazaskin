30f1b6700178bab4aebe08b7a7fc757c92505175
chikiet88 <chikiet88@gmail.com>
2024-04-24 08:24:52 +0700

    update

--- apps/api/src/app/shared/telegram.service.ts
+++ apps/api/src/app/shared/telegram.service.ts
@@ -22,22 +22,10 @@ export class TelegramService {
   }
   async SendMiniAppLogdev(data: any): Promise<any> {
     const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        chat_id: environment.APITelegram_LogMiniApp,
-        text: data,
-        parse_mode: 'html',
-      }),
+      url: `https://api.telegram.org/bot${environment.APITelegram_accesstoken}/sendMessage?chat_id=${environment.APITelegram_LogMiniApp}&text=${data}&parse_mode=html`,
     };
-    //fetch telegram
-    const response = await fetch(`https://api.telegram.org/bot${environment.APITelegram_accesstoken}/sendMessage`, options);
-    if (!response.ok) {
-      throw new Error(`Error fetching data: ${response.statusText}`);
-    }
-    return response.json();
+    const response = await axios.request(options);
+    return response.data;
   }
   async SendDulieuVttech(data: string): Promise<any> {
     const options = {
