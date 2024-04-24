d0b04a4c0760f5ca2b37cd06c57e6ec2dc433615
chikiet88 <chikiet88@gmail.com>
2024-04-24 10:34:57 +0700

    update

--- apps/api/src/app/shared/shared.service.ts
+++ apps/api/src/app/shared/shared.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 import moment = require('moment');
+import axios from 'axios';
 @Injectable()
 export class SharedService {
   constructor(private _TelegramService:TelegramService) {}
-  async getToken(item: any) { 
+  async getToken(item: any) {
     try {
-      const response = await fetch('https://apismsvtt.vttechsolution.com/api/Client/Autho', {
-        method: 'POST',
-        credentials: 'include',
+      const response = await axios.post('https://apismsvtt.vttechsolution.com/api/Client/Autho', item && typeof item === 'object' && Object.keys(item).length > 0 ? item : {"Name": "Taza","Password": "1b9287d492b256x7taza","Type": "web"}, {
+        withCredentials: true,
         headers: {
           'Content-Type': 'application/json',
         },
-        body: item && typeof item === 'object' && Object.keys(item).length > 0 ? JSON.stringify(item) : JSON.stringify({"Name": "Taza","Password": "1b9287d492b256x7taza","Type": "web"}),
       });
-      const data = await response.json();
-      const cookies = response.headers.get('set-cookie');
+      const data = response.data;
+      const cookies = response.headers['set-cookie'];
       switch (data.Status) {
         case 1:
-          return [data,cookies];
+          return [data, cookies];
         default:
           this._TelegramService.SendMiniAppLogdev(`Lỗi Xác Thực ${JSON.stringify(item)} Vttech ${moment().format("HH:mm:ss DD/MM/YYYY")}`);
-          return data
+          return data;
       }
     } catch (error) {
-     return  this._TelegramService.SendMiniAppLogdev(`Lỗi Xác Thực ${error} Vttech ${moment().format("HH:mm:ss DD/MM/YYYY")}`);
+      return this._TelegramService.SendMiniAppLogdev(`Lỗi Xác Thực ${error} Vttech ${moment().format("HH:mm:ss DD/MM/YYYY")}`);
     }
   }
 }
