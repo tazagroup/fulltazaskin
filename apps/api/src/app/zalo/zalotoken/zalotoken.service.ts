1c7227d2b18e413712bd4c9a8f2d4af560601666
chikiet88 <chikiet88@gmail.com>
2024-04-25 10:03:59 +0700

    update

--- apps/api/src/app/zalo/zalotoken/zalotoken.service.ts
+++ apps/api/src/app/zalo/zalotoken/zalotoken.service.ts
@@ -31,7 +31,7 @@ export class ZalotokenService {
     try {
       const response = await axios.post('https://oauth.zaloapp.com/v4/oa/access_token', options);
       const data = response.data;
-      console.error(data);
+      return data
       if(data.error == '0')
         {
             delete item.code;
