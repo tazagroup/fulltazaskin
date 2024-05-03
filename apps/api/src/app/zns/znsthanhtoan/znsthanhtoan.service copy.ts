9ac9aed24a4cdf8646333678423272518fd33b97
chikiet88 <chikiet88@gmail.com>
2024-05-03 13:03:24 +0700

    update

---
+++ apps/api/src/app/zns/znsthanhtoan/znsthanhtoan.service copy.ts
@@ -0,0 +1,231 @@
+// import { Injectable } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { Like, Repository } from 'typeorm';
+// import { ZnsthanhtoanEntity } from './entities/znsthanhtoan.entity';
+// import { VttechthanhtoanService } from '../../vttech/vttechthanhtoan/vttechthanhtoan.service';
+// import { TelegramService } from '../../shared/telegram.service';
+// import moment = require('moment');
+// import { ChinhanhService } from '../../cauhinh/chinhanh/chinhanh.service';
+// import { DescErrorZalo, GenId, convertPhoneNum } from '../../shared.utils';
+// import { ZaloznstrackingService } from '../../zalo/zaloznstracking/zaloznstracking.service';
+// @Injectable()
+// export class ZnsthanhtoanService {
+//   constructor(
+//     @InjectRepository(ZnsthanhtoanEntity)
+//     private ZnsthanhtoanRepository: Repository<ZnsthanhtoanEntity>,
+//     private _VttechthanhtoanService: VttechthanhtoanService,
+//     private _TelegramService: TelegramService,
+//     private _ChinhanhService: ChinhanhService,
+//     private _ZaloznstrackingService: ZaloznstrackingService,
+//   ) { }
+//   async createzns(data: any) {
+//     const Thanhtoans = await this._VttechthanhtoanService.findQuery(data);
+//     if (Thanhtoans.length > 0) {
+//       let CountCreate = 0;
+//       await Promise.all(Thanhtoans.map(async (v: any, k: any) => {
+//         const item: any = {};
+//         item.Dulieu = v;
+//         item.CustPhone = v.CustPhone;
+//         item.CustName = v.CustName;
+//         item.CustCode = v.CustCode;
+//         item.BranchID = v.BranchID;
+//         item.Created = moment(v.Created).format('YYYY-MM-DD');
+//         item.Paid = v.Paid;
+//         item.Code = v.Code;
+//         const isCreate = await this.create(item);
+//         console.log(isCreate);
+//         if (isCreate.error !== 1001) {
+//           CountCreate = CountCreate + 1;
+//         }
+//       }));
+//       this._TelegramService.SendMiniAppLogdev(`[ZNS_THANHTOAN] - Step2 - Create (${CountCreate}) Thanh Toan - ${moment().format('HH:mm:ss DD/MM/YYYY')}`);
+//     }
+//     return Thanhtoans;
+//   }
+
+//   async getTemplateData(id: any, token: any) {
+//     try {
+//       const response = await fetch(`https://business.openapi.zalo.me/template/info?template_id=${id}`, {
+//         method: 'GET',
+//         headers: {
+//           'access_token': token
+//         }
+//       });
+//       const data = await response.json();
+//       return data;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+//   async sendsms(data: any) {
+//     try {
+//       const response = await fetch('https://sms.cmctelecom.vn/SMS_CMCTelecom/api/sms/sendutf', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//       });
+//       const responseData = await response.json();
+//       return responseData;
+//     } catch (error) {
+//       return error;
+//     }
+//   }
+
+//   async sendzns(data: any) {
+//     console.log(data);
+//     const Chinhanh: any = await this._ChinhanhService.findbyidVttech(data.BranchID)
+//     console.log(Chinhanh);
+//     try {
+//       if (!Chinhanh?.ZaloOaToken?.access_token) {
+//         this._TelegramService.SendMiniAppLogdev(`[ZNS_THANHTOAN] - ${data.BranchID} - ${Chinhanh?.Title} - Chưa Có Token - ${moment().format('HH:mm:ss DD/MM/YYYY')}`);
+//       }
+//       else {
+//         const priceProperty = Chinhanh.TemplateThanhtoan === '301891' || Chinhanh.TemplateThanhtoan === '302259' ? 'price' : 'cost';
+//         const requestData = {
+//          // mode: "development",
+//           phone: convertPhoneNum(data.CustPhone),
+//           template_id: Chinhanh.TemplateThanhtoan,
+//           template_data: {
+//             order_code: data.Code,
+//             note: moment(data.Created).format('DD/MM/YYYY'),
+//             [priceProperty]: parseFloat(data.Paid).toFixed(0),
+//             customer_name: data.CustName,
+//           },
+//           tracking_id: GenId(12, true),
+//         };
+//         const config = {
+//           method: 'post',
+//           headers: {
+//             'access_token': Chinhanh.ZaloOaToken.access_token,
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify(requestData)
+//         };
+//         // if (data.CustPhone == "0977272967") {
+//         const response = await fetch(`https://business.openapi.zalo.me/message/template`, config);
+//         if (!response.ok) {
+//           throw new Error(`Error fetching data: ${response.statusText}`);
+//         }
+//         const result = await response.json();
+//         console.log(result);
+//         this._TelegramService.SendMiniAppLogdev(`[ZNS_THANHTOAN] - ${result.error} - ${DescErrorZalo(result.error)} - ${Chinhanh.Title} - ${data.CustName} - ${data.CustPhone} - ${data.Code} - ${data.Paid} - ${moment().format('HH:mm:ss DD/MM/YYYY')}`);
+//         if (result.error == 0) {
+//           data.Status = 1;
+//           data.message_id =result.data.message_id;
+//           data.trackingId =requestData.tracking_id;
+//           this.update(data.id, data)
+//         }
+//         else {
+//           data.Status = 2;
+//           data.Statuscode = result.error;
+//           // const resultsms = await this.sendsms({
+//           //   "Brandname": "TAZA",
+//           //   "Message": `${data.CustName} da thanh toan so tien ${data.Paid} co ma hoa don la ${data.Code}. Taza cam on quy khach`,
+//           //   "Phonenumber": data.CustPhone,
+//           //   "user": "ctytaza2",
+//           //   "pass": "$2a$10$QjKAPJ9qq.RuS3jfUID2FeuGdpuSL1Rl9ugQUvy.O5PuKSlp8z95S",
+//           //   "messageId": data.CustPhone + (new Date()).getTime()
+//           // })
+//           // console.log(resultsms);
+//           // data.SMSCode = resultsms.data.status;
+//           // data.messageId =resultsms.data.messageId;
+//           this.update(data.id, data)
+//         }
+
+//         return result
+//         // }
+//       }
+//     } catch (error) {
+//       throw error; // Rethrow for proper error propagation
+//     }
+//   }
+//   async sendznsauto(data: any) {
+//     data.CreatedBegin?data.CreatedBegin = data.CreatedBegin:moment().format('YYYY-MM-DD');
+//     data.createdEnd?data.createdEnd = data.createdEnd:moment().format('YYYY-MM-DD');
+//     data.Status?data.Status = data.Status:0;
+//     const result = await this.findQuery(data)
+//     return result
+    
+//   }
+//   async create(data: any) {
+//     const check = await this.findSHD(data)
+//     if (!check) {
+//       this.ZnsthanhtoanRepository.create(data);
+//       return await this.ZnsthanhtoanRepository.save(data);
+//     }
+//     else {
+//       return { error: 1001, data: "Trùng Dữ Liệu" }
+//     }
+
+//   }
+
+//   async findAll() {
+//     return await this.ZnsthanhtoanRepository.find();
+//   }
+//   async findid(id: string) {
+//     return await this.ZnsthanhtoanRepository.findOne({ where: { id: id } });
+//   }
+//   async findSHD(data: any) {
+//     return await this.ZnsthanhtoanRepository.findOne({
+//       where: {
+//         Code: data.Code,
+//         CustPhone: data.CustPhone
+//       },
+//     });
+//   }
+//   async findslug(Code: any) {
+//     return await this.ZnsthanhtoanRepository.findOne({
+//       where: { Code: Code },
+//     });
+//   }
+//   async findPagination(page: number, perPage: number) {
+//     const skip = (page - 1) * perPage;
+//     const totalItems = await this.ZnsthanhtoanRepository.count();
+//     const znsthanhtoans = await this.ZnsthanhtoanRepository.find({ skip, take: perPage });
+//     return {
+//       currentPage: page,
+//       perPage,
+//       totalItems,
+//       totalPages: Math.ceil(totalItems / perPage),
+//       data: znsthanhtoans,
+//     };
+//   }
+//   async findQuery(params: any) {
+//     const queryBuilder = this.ZnsthanhtoanRepository.createQueryBuilder('znsthanhtoan');
+//     if (params.hasOwnProperty('CreatedBegin') && params.hasOwnProperty('CreatedEnd')) {
+//       queryBuilder.andWhere('znsthanhtoan.Created BETWEEN :startDate AND :endDate', {
+//         startDate: params.CreatedBegin,
+//         endDate: params.CreatedEnd,
+//       });
+//     }
+//     if (params.hasOwnProperty('Batdau') && params.hasOwnProperty('Ketthuc')) {
+//       queryBuilder.andWhere('znsthanhtoan.CreateAt BETWEEN :startDate AND :endDate', {
+//         startDate: params.Batdau,
+//         endDate: params.Ketthuc,
+//       });
+//     }
+//     if (params.hasOwnProperty('Title')) {
+//       queryBuilder.andWhere('znsthanhtoan.Title LIKE :Title', { SDT: `%${params.Title}%` });
+//     }
+//     if (params.hasOwnProperty('Status')) {
+//       queryBuilder.andWhere('znsthanhtoan.Status = :Status', { Status: `${params.Status}` });
+//     }
+//     const [items, totalCount] = await queryBuilder
+//       .limit(params.pageSize || 10) // Set a default page size if not provided
+//       .offset(params.pageNumber * params.pageSize || 0)
+//       .getManyAndCount();
+//     console.log(items, totalCount);
+//     return { items, totalCount };
+//   }
+//   async update(id: string, UpdateZnsthanhtoanDto: any) {
+//     this.ZnsthanhtoanRepository.save(UpdateZnsthanhtoanDto);
+//     return await this.ZnsthanhtoanRepository.findOne({ where: { id: id } });
+//   }
+//   async remove(id: string) {
+//     console.error(id)
+//     await this.ZnsthanhtoanRepository.delete(id);
+//     return { deleted: true };
+//   }
+// }
