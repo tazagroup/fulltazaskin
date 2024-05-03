badc36eece1cacafec966e2a9c1c67e5e04c682b
chikiet88 <chikiet88@gmail.com>
2024-05-03 13:23:15 +0700

    update

--- apps/api/src/app/zns/znsdieutri/znsdieutri.service.ts
+++ apps/api/src/app/zns/znsdieutri/znsdieutri.service.ts
@@ -129,14 +129,9 @@ export class ZnsdieutriService {
         return result
       }
     } catch (error) {
-      if (error.response && error.response.status === 429) {
         this._TelegramService.SendMiniAppLogdev(`[ZNS_DIEUTRI] - Mã Lỗi 3:  ${JSON.stringify(error)}`);
         await new Promise(resolve => setTimeout(resolve, 5000));
         return this.sendzns(data); // Retry the request
-      }
-      else {
-        this._TelegramService.SendMiniAppLogdev(`[ZNS_DIEUTRI] - Mã Lỗi 4:  ${JSON.stringify(error)}`);
-      }
     }
   }
   // async sendznsauto(data: any) {
