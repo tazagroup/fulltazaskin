bb8832a4a1ea4a57c7d51e38578002a6684aa2e9
chikiet88 <chikiet88@gmail.com>
2024-05-03 13:07:42 +0700

    update

--- apps/api/src/app/zns/znsdieutri/znsdieutri.service.ts
+++ apps/api/src/app/zns/znsdieutri/znsdieutri.service.ts
@@ -114,7 +114,7 @@ export class ZnsdieutriService {
         }
         const result = await response.json();
         console.log(result);
-        this._TelegramService.SendMiniAppLogdev(`[ZNS_DIEUTRI] - Mã Lỗi 2 :  ${result.error} - ${DescErrorZalo(result.error)} - ${Chinhanh.Title} - ${data.CustName} - ${data.CustPhone} - ${data.Code} - ${data.Paid} - ${moment().format('HH:mm:ss DD/MM/YYYY')}`);
+        this._TelegramService.SendMiniAppLogdev(`[ZNS_DIEUTRI] - Mã Lỗi 2 :  ${result.error} - ${DescErrorZalo(result.error)} - ${Chinhanh.Title} - ${data.CustName} - ${data.CustPhone} - ${data.Code} - ${data.Paid} - ${moment().format('HH:mm:ss DD/MM/YYYY')} ${JSON.stringify(result)}`);
         if (result.error == 0) {
           data.Status = 1;
           data.messageId =result.data.msg_id;
