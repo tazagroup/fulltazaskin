87de32e76e71ba819bbf490e5c51a13064b9c8a6
chikiet88 <chikiet88@gmail.com>
2024-08-13 10:37:25 +0700

    update

--- apps/api/src/app/rediscache.service.ts
+++ apps/api/src/app/rediscache.service.ts
@@ -1,34 +1,25 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
-import Redis from 'ioredis';
 @Injectable()
 export class RediscacheService {
-  private redis: Redis;
-  constructor(
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
-  ) {
-    this.redis = new Redis();
-    // this.redis = new Redis('/home/tazaspac/redis/redis.sock');
-  }
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
   async getDataWithCache(key: string,data:any): Promise<any> {
-    const cachedData = await this.redis.get(key);
-    console.error(cachedData);
+    const cachedData = await this.cacheManager.get(key);
     if (cachedData) {
       return cachedData;
     }
     else
     {
-      await this.redis.set(key, data,'EX', 60*5); // Cache for 60 seconds
+      await this.cacheManager.set(key, data, 60*5); // Cache for 60 seconds
       return data;
     }
 
   }
-  async set(key: string, value: string, expiration: number) {
-    await this.redis.set(key, value, 'EX', expiration);
-  }
 
-  async get(key: string) {
-    return await this.redis.get(key);
+  private async fetchDataFromDataSource(): Promise<any> {
+    // Replace with your actual data fetching logic
+    return { data: 'fetched from data source' };
   }
 }
