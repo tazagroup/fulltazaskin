12b56fab32beff3b7e335bf8dbb2949758e82b37
chikiet88 <chikiet88@gmail.com>
2024-08-24 13:15:13 +0700

    update

--- apps/api/src/app/zns/znsthanhtoan/znsthanhtoan.controller.ts
+++ apps/api/src/app/zns/znsthanhtoan/znsthanhtoan.controller.ts
@@ -9,7 +9,7 @@ export class ZnsthanhtoanController {
     private readonly znsthanhtoanService:ZnsthanhtoanService,
     private readonly _LoggerService:LoggerService,
   ) {}
-  //@Interval(1320000)
+  @Interval(1320000)
   @Post('createzns')
   createzns(@Body() data: any={}) {
     console.error('Tạo ZNS Thanh Toán 2',moment().format('YYYY-MM-DD HH:mm:ss'));
