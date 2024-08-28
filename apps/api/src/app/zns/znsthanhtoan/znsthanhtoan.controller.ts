8b1f9ce964d451550a618dae1d11b84fe0066e99
quocbao1983 <quocbao280783@gmail.com>
2024-08-28 21:20:11 +0700

    update

--- apps/api/src/app/zns/znsthanhtoan/znsthanhtoan.controller.ts
+++ apps/api/src/app/zns/znsthanhtoan/znsthanhtoan.controller.ts
@@ -29,7 +29,7 @@ export class ZnsthanhtoanController {
     console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
 
   }
-  @Interval(60*60*1000)
+  //@Interval(60*60*1000)
   @Post('sendznsauto')
   async sendznsauto(@Body() data: any={}) {
     console.error('Gửi ZNS Thanh Toán Auto 3',moment().format('YYYY-MM-DD HH:mm:ss'));
@@ -61,7 +61,7 @@ export class ZnsthanhtoanController {
     }
     else  return "Không thể gửi tin nhắn vào thời gian này";
   }
-  @Cron('00 45 21 * * *')
+  //@Cron('00 45 21 * * *')
   @Post('thanhtoanenday')
   async sendznsautoCron(@Body() data: any={}) {
     console.error('Gửi ZNS Thanh Toán Auto Cuối Ngày 4',moment().format('YYYY-MM-DD HH:mm:ss'));
